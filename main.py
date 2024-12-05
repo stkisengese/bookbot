7170a80d7dc4694f86e9f51e23ee6b7a7c0a638e
stkisengese <skisengese@outlook.com>
2024-12-05 19:18:15 +0300

    feat: add character count feature

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+
 def main():
     file_paths = "books/frankenstein.txt"
     with open(file_paths) as f:
         file_contents = f.read()
 
         print(count_words(file_contents))
+        print(f"Count Characters: {count_characters(file_contents)}")
 
 def count_words(words):
     content = words.split()
     return len(content)
+
+def count_characters(text):
+    char_counts = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        char_counts[char] = char_counts.get(char, 0) + 1
+    return char_counts
 
 
 main()
