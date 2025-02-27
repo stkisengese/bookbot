a6fe0955a9f111691fcf8d17f154f3e1866e57de
skisenge <skisengese@gmail.com>
2025-02-27 20:27:49 +0300

    chore: improve code structure

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import count_words
 
 def main():
     file_paths = "books/frankenstein.txt"
@@ -5,35 +6,9 @@ def main():
         file_contents = f.read()
 
     word_count = count_words(file_contents)
-    char_counts = count_characters(file_contents)
+    # char_counts = count_characters(file_contents)
 
-    print_report(word_count, char_counts)
+    # print_report(word_count, char_counts)
+    print(f"{word_count} words found in the document")
 
-def count_words(words):
-    content = words.split()
-    return len(content)
-
-def count_characters(text):
-    char_counts = {}
-    lower_text = text.lower()
-    for char in lower_text:
-        char_counts[char] = char_counts.get(char, 0) + 1
-    return char_counts
-
-def sort_on_count(char_count):
-    return char_count[1]
-
-def print_report(word_count, char_counts):
-
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{word_count} words found in the document\n")
-    
-    sorted_char = sorted(char_counts.items(), key=sort_on_count, reverse=True)
-
-    for char, count in sorted_char:
-        if char.isalpha():
-            print(f"The '{char}' character was found {count} times")
-
-    print("--- End report ---")
-    
 main()
