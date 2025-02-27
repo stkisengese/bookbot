8d5b52ea3ea4dc5584fe706d380b5e01c02b915c
skisenge <skisengese@gmail.com>
2025-02-27 20:46:07 +0300

    feat: update bookbot output analysis

--- stats.py
+++ stats.py
@@ -3,26 +3,28 @@ def count_words(words):
     content = words.split()
     return len(content)
 
-# def count_characters(text):
-#     char_counts = {}
-#     lower_text = text.lower()
-#     for char in lower_text:
-#         char_counts[char] = char_counts.get(char, 0) + 1
-#     return char_counts
+def count_characters(text):
+    char_counts = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        char_counts[char] = char_counts.get(char, 0) + 1
+    return char_counts
 
-# def sort_on_count(char_count):
-#     return char_count[1]
+def sort_on_count(char_count):
+    return char_count[1]
 
-# def print_report(word_count, char_counts):
-
-#     print("--- Begin report of books/frankenstein.txt ---")
-#     print(f"{word_count} words found in the document\n")
+def print_report(word_count, char_counts):
+    print("============ BOOKBOT ===========")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count -------------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count ----------")
     
-#     sorted_char = sorted(char_counts.items(), key=sort_on_count, reverse=True)
+    sorted_char = sorted(char_counts.items(), key=sort_on_count, reverse=True)
 
-#     for char, count in sorted_char:
-#         if char.isalpha():
-#             print(f"The '{char}' character was found {count} times")
+    for char, count in sorted_char:
+        if char.isalpha():
+            print(f"{char}: {count}")
 
-#     print("--- End report ---")
+    print("================ End ================")
     
