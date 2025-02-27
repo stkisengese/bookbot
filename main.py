b23e12d0e84b0406f0cac433c928556889c1374e
skisenge <skisengese@gmail.com>
2025-02-27 20:47:17 +0300

    feat: print new output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, print_report
 
 def main():
     file_paths = "books/frankenstein.txt"
@@ -6,9 +6,9 @@ def main():
         file_contents = f.read()
 
     word_count = count_words(file_contents)
-    # char_counts = count_characters(file_contents)
+    char_counts = count_characters(file_contents)
 
-    # print_report(word_count, char_counts)
-    print(f"{word_count} words found in the document")
+    print_report(word_count, char_counts)
+
 
 main()
